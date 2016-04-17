40fd606e4451acc86200667962e69958f696ddc2
Claude Seguret <claude.seguret@laposte.fr>
2016-04-17 08:56:56 +0200

    initial commit

---
+++ api/services/generateUID.js
@@ -0,0 +1,17 @@
+// use generateUID.generate(req,error,"ERR_500_004");
+// generate a random id with comprehensive begining like lolipo12
+module.exports = {
+
+    generate: function() {
+
+      var randomId =shortid.generate();
+      randomId=randomId.replace('_', 'A');
+      randomId=randomId.replace('-', 'b');
+
+      var consonnes = ['b','c','d','f','g','j','k','l','m','n','p','r','s','t','v','x','z'];
+      var voyelles = ['a', 'e', 'i', 'o', 'u'];
+      var desChiffresEtDesLettres = consonnes[Math.floor(Math.random() * 17)] + voyelles[Math.floor(Math.random() * 5)] + consonnes[Math.floor(Math.random() * 17)] + voyelles[Math.floor(Math.random() * 5)] +consonnes[Math.floor(Math.random() * 17)] + voyelles[Math.floor(Math.random() * 5)] + ('00' +Math.floor(Math.random() * 100)).slice(-3);
+      randomId = desChiffresEtDesLettres + randomId;
+      return randomId;
+    }
+};
